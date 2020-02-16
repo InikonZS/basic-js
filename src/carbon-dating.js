f2c4a72b654f585fee4249979ddaeb70a6b05c88
Inikon <ZhanZS@mail.ru>
2020-02-16 03:41:14 +0300

    feat: solve someting and fall asleep

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
+  console.log(sampleActivity);
+  if (!sampleActivity) {return false;}
+  if ((typeof sampleActivity) !== 'string') {return false;}
+  if (isNaN(parseFloat(sampleActivity))) {return false;}
+  sampleActivity=parseFloat(sampleActivity);
+  if (+sampleActivity>MODERN_ACTIVITY) {return false;}
+  if (+sampleActivity<=0) {return false;}
+  var res=Math.ceil((Math.log2(MODERN_ACTIVITY)-Math.log2(sampleActivity))*5730*1.000214);
+  console.log(res)
+  return res; 
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  res=[];
+  if (!members){ return false}
+  if (typeof members != "object") {return false}
+  if (members.isArray==false) {return false}
+  for (let i=0;i<members.length;i++){
+    if (typeof members[i] =="string"){
+      if (members[i].length>0){
+      res.push(members[i].trim()[0].toUpperCase());
+      }
+    }
+  }
+  return res.sort().join("");
   // remove line with error and write your code here
 };

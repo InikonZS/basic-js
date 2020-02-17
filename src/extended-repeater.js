2c62a3a44bc71503d6fcc585bea8ded90bbe3f73
Inikon <ZhanZS@mail.ru>
2020-02-17 18:04:12 +0300

    fix: yeah, I made it

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
+module.exports = function repeater(str, options) {
+    var res="";
+    var addi="";
+    if (options.addition===undefined) {options.addition=""}
+    if (options.additionSeparator===undefined) {options.additionSeparator="|"}
+    if (options.separator===undefined) {options.separator="+"}
+    for (let i=0;i<options.additionRepeatTimes-1;i++){
+        addi+=options.addition+options.additionSeparator;
+    }
+    addi+=options.addition;
+    str=str+addi;
+    for (let i=0;i<options.repeatTimes-1;i++){
+        res+=str+options.separator;
+    }
+    res+=str;
+    return res;
+    //агусик покакунькал
+/*repeatTimes
+separator
+addition
+additionRepeatTimes
+additionSeparator*/
     // remove line with error and write your code here
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
+    constructor (type){
+        this.type=type;
+    }
+    encrypt(message, key) {
+        
+        return this.crypt(message,key,true);
+       // throw 'Not implemented';
         // remove line with error and write your code here
     }
 
-    decrypt() {
-        throw 'Not implemented';
+    decrypt(encryptedMessage, key) {
+        return this.crypt(encryptedMessage,key,false);
+        //throw 'Not implemented';
         // remove line with error and write your code here
     }
+
+    crypt(m, k, un){
+        if (!m||!k){throw Error}
+        //if (this.type===false){m.split("").reverse().join("");}
+        var alf=[];
+        for (let i=0;i<26;i++){
+            alf.push(i+65);
+        }
+        alf=String.fromCharCode(...alf);
+        
+        //console.log(m);
+        var kr="";
+        var mr="";
+        while (kr.length<m.length){kr+=k}
+        kr=kr.toUpperCase();
+        var j=-1;
+        for (let i=0;i<m.length;i++){
+            let ind;
+            if (alf.indexOf(m[i].toUpperCase())!=-1){
+                j++;
+            }
+            if (un==true){
+                ind=alf.indexOf(kr[j])+alf.indexOf(m[i].toUpperCase());
+                ind=ind%26;
+            }
+            else{
+                ind=-alf.indexOf(kr[j])+alf.indexOf(m[i].toUpperCase());
+                if (ind<0){ind=26+ind;}
+                //ind=ind%26;
+            }
+            //if (ind<0){ind=26+ind;}
+            if (alf.indexOf(m[i].toUpperCase())!=-1){
+                //if (m[i].toUpperCase()==m[i]) {mr+=alf[ind];} else {mr+=(""+alf[ind]).toLowerCase();}
+                mr+=alf[ind];
+            } else {
+                mr+=m[i];
+            }
+        }
+        //console.log(mr);
+        if (this.type===false){mr=mr.split("").reverse().join("");}
+        return mr;
+    }
+    
 }
 
 module.exports = VigenereCipheringMachine;
